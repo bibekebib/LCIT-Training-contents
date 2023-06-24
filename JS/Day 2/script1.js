cc4536c05fba10a25955f84804eb732485292a65
Bibek Chalise <modernbib@gmail.com>
2023-06-24 22:44:02 +0545

    day 3 js arrays and for loop

---
+++ JS/Day 2/script1.js
@@ -0,0 +1,14 @@
+alert('this is awesome')
+
+
+
+document.getElementById('demo').innerHTML = "hello world";
+document.getElementById('demo').innerHTML = 8 + 10;
+
+document.getElementById('demo1').innerHTML = '<a href="hello.html">New page</a>'
+
+document.getElementById('demo').textContent = "This is new way"
+
+document.write('hello csit')
+
+document.getElementById('demo').style.color = "red"
---
+++ JS/Day 3/script2.js
@@ -0,0 +1,62 @@
+var arr = [1, 2, 4, 10, 15]
+console.log(arr.length)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var age = 16;
+// var gender = 'female';
+
+// if (gender=='male'){
+//     if (age > 25){
+//         console.log('can marry')
+//     }
+//     else{
+//         console.log('cant marry')
+//     }
+// }
+// else if (gender=='female'){
+//     if (age > 24){
+//         console.log('can marry')
+//     }
+//     else{
+//         console.log('cant marry')
+//     }
+// }
+
+
+
+
+
+
+
+
+// if (age < 18){
+//     console.log('cant vote')
+//     document.getElementById('demo1').innerHTML = "cant vote"
+// }
+// else if(age > 18){
+//     console.log('can vote')
+//     document.getElementById('demo1').innerHTML = "can vote"
+// }
+// else {
+//     console.log('voter id xa?')
+//     document.getElementById('demo1').innerHTML = 'voter id xa?'
+// }
