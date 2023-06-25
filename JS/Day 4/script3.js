d49c9ea9e0295b3cfe23addb830efa96f08bed7a
Bibek Chalise <modernbib@gmail.com>
2023-06-25 16:43:41 +0545

    arrays and loops

---
+++ JS/Day 4/script3.js
@@ -0,0 +1,95 @@
+var arr = [1, 2, 3, 'bibek', true, false, 'Yes', 'Jpt']
+// access elements of js array
+console.log(arr[7])
+
+console.log(arr.length)
+
+arr.push('good morning')
+console.log(arr)
+console.log(arr.length)
+
+arr.pop()
+console.log(arr)
+
+arr.shift()
+console.log(arr)
+
+arr.unshift('great')
+console.log(arr)
+
+arr[4] = 'csit'
+console.log(arr)
+
+var arr1 = ['bca', 'bim', 'gaindakot']
+var arr2 = ['csitI', 'csitII', 'csitIII']
+
+arr1 = arr1.concat(arr, arr2)
+console.log(arr1)
+
+for (let i = 0; i<14; i++){
+    console.log(i)
+    console.log(arr1[i])
+}
+
+var i = 0;
+while (i < 5) {
+    console.log(" while wala output The number is " , i)
+    i++;
+  }
+
+  var i = 0;
+  do {
+    console.log(" do while wala The number is " + i)
+    i++;
+  }
+  while (i < 5);
+
+
+
+
+
+
+
+
+
+
+// arr.push('asim')
+// console.log(arr)
+
+// arr[4] = 'sandeep'
+// console.log(arr)
+
+// arr[4] = 'lict'
+// console.log(arr)
+
+// removed = arr.pop()
+
+// console.log(removed)
+// console.log(arr)
+
+// arr.shift()
+// console.log(arr)
+// arr.unshift('great')
+// console.log(arr)
+
+// var arr1 = ['bca', 'csit', 'bim']
+
+// arr1 = arr1.concat(arr)
+// console.log(arr1)
+
+// for (let i=0; i<=5;i++){
+//     console.log(i)
+//     console.log(arr1[i])
+// }
+
+// var i = 0;
+// while (i < 10) {
+//     console.log("The number is " + i)
+//     i++;
+//   }
+//   var i = 0;
+//   do {
+//     console.log("The number is " + i)
+//     i++;
+//   }
+//   while (i < 10);
